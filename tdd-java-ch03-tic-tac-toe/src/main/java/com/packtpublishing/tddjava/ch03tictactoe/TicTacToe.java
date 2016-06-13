b127d36208829bef30a94c2c67aac8ef81e99835
jimmy.do0704 <jimmy.do0704@gmail.com>
2016-06-13 16:45:55 +0700

    update to req 1

--- tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -4,8 +4,37 @@ package com.packtpublishing.tddjava.ch03tictactoe;
  * Created by dvdat on 6/10/16.
  */
 public class TicTacToe {
-    public void placeX(int i) {
-        if(i > 3)
-            throw new RuntimeException("Place outside the board");
+    private static char empty = ' ';
+    public static char playX = 'X';
+    public static char playO = 'O';
+
+    private char [][] board = {{empty, empty, empty},
+            {empty, empty, empty}, {empty, empty, empty}};
+
+    private char lastPlayer = empty;
+
+    public void play(int x, int y) {
+        if(x < 1 || x > 3)
+            throw new RuntimeException("X outside the board");
+
+        if(y < 1 || y > 3)
+            throw new RuntimeException("Y outside the board");
+
+        setBox(x, y);
+        lastPlayer = nextPlayer();
+    }
+
+    private void setBox(int x, int y) {
+        if(board[x - 1][y - 1] != empty)
+            throw new RuntimeException("Already occurred");
+        else
+            board[x - 1][y - 1] = playX;
+    }
+
+    public char nextPlayer() {
+        if(lastPlayer == playX)
+            return playO;
+
+        return playX;
     }
 }
--- tdd-java-ch03-tic-tac-toe/src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ tdd-java-ch03-tic-tac-toe/src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -1,5 +1,6 @@
 package com.packtpublishing.tddjava.ch03tictactoe;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,8 +20,39 @@ public class TicTacToeSpec {
     }
 
     @Test
-    public void whenPlacePieceOutsideXThrowRuntimeException() {
+    public void whenXOutsideThenRuntimeException() {
         expectedException.expect(RuntimeException.class);
-        instance.placeX(3);
+        instance.play(4, 1);
+    }
+
+    @Test
+    public void whenYOutsideThenRuntimeException() {
+        expectedException.expect(RuntimeException.class);
+        instance.play(1, 5);
+    }
+
+    @Test
+    public void whenOccupiedThenRuntimeException() {
+        instance.play(1, 2);
+        expectedException.expect(RuntimeException.class);
+        instance.play(1, 2);
+    }
+
+    @Test
+    public void givenFirstPlayerThenNextPlayerX() {
+        Assert.assertEquals(TicTacToe.playX, instance.nextPlayer());
+    }
+
+    @Test
+    public void givenPlayerXThenNextPlayerO() {
+        instance.play(1, 1);
+        Assert.assertEquals(TicTacToe.playO, instance.nextPlayer());
+    }
+
+    @Test
+    public void givenLastPlayerOThenNextPlayerX() {
+        instance.play(1, 1);
+        instance.play(1, 2);
+        Assert.assertEquals(TicTacToe.playX, instance.nextPlayer());
     }
 }
